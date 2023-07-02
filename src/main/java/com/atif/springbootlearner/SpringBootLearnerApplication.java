f42f21e2eba2ac9d27f39a9bd5c3260844e1528a
Atif Saleed <atif.mohd.sld@gmail.com>
2023-07-02 11:04:11 +0530

    Project created using Spring Initializr

---
+++ src/main/java/com/atif/springbootlearner/SpringBootLearnerApplication.java
@@ -0,0 +1,13 @@
+package com.atif.springbootlearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atif/springbootlearner/SpringBootLearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atif.springbootlearner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

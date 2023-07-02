e1417277a4ada23a6b42af174d8f05e2c0738707
Atif Saleed <atif.mohd.sld@gmail.com>
2023-07-02 11:16:00 +0530

    Book Entity created

---
+++ src/main/java/entity/Book.java
@@ -0,0 +1,87 @@
+package entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Book {
+    @Id
+    @SequenceGenerator(
+            name = "book_id_sequence",
+            sequenceName = "book_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "book_id_sequence"
+    )
+    private Integer id;
+    private String name;
+    private String author;
+    private Integer year;
+
+    public Book() {
+    }
+
+    public Book(Integer id, String name, String author, Integer year) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(name, book.name) && Objects.equals(author, book.author) && Objects.equals(year, book.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}

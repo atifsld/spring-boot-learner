868294dfd1bc476a82789c8cb87b35bb6f3c78e7
Atif Saleed <atif.mohd.sld@gmail.com>
2023-07-12 17:19:07 +0530

    Removed unnecessary imports

--- src/main/java/com/atif/springbootlearner/controller/BookController.java
+++ src/main/java/com/atif/springbootlearner/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.atif.springbootlearner.controller;
 
 import com.atif.springbootlearner.entity.Book;
-import com.atif.springbootlearner.repository.BookRepository;
 import com.atif.springbootlearner.service.BookService;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/atif/springbootlearner/service/BookService.java
+++ src/main/java/com/atif/springbootlearner/service/BookService.java
@@ -3,7 +3,6 @@ package com.atif.springbootlearner.service;
 import com.atif.springbootlearner.entity.Book;
 import com.atif.springbootlearner.repository.BookRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
